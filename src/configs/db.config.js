c1cec8de60796a2d4cad3abf3fdaca3011761096
Destiny Ifehor <Destiny.Ifehor@interswitchgroup.com>
2023-09-17 23:28:13 +0100

    updated

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -5,10 +5,15 @@ mongoose.Promise = global.Promise;
 
 exports.DBConfiguration = () => {
   mongoose
-    .connect(process.env.MONGOOSE_URL, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    })
+    .connect(
+      process.env.NODE_ENV === "production"
+        ? process.env.MONGO_URL
+        : process.env.MONGOOSE_URL,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    )
     .then(() => console.log("DB connected"))
     .catch((err) => console.log(err.message, "DB error"));
 };
